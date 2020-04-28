d694d75f7316908b86866fb5c7a741116f28309b
John Leintz <johnleintz@scaledflow.com>
2020-04-28 15:08:49 -0500

    added date functions

---
+++ script.js
@@ -0,0 +1,14 @@
+var month = getMonth();
+console.log(month);
+
+var day = getDay();
+console.log(day);
+
+var year = getYear();
+console.log(year);
+
+var ord = getDayOrdinal();
+console.log(ord);
+
+var hour = getHour();
+console.log(hour);
